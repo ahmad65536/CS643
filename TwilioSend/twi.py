e9adce47be40cb40a448f76c29d7208d576f9a32
root <Haseeb ur Rahman>
2017-10-04 18:15:42 +0000

    move to new directory

---
+++ TwilioSend/twi.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from twilio.rest import Client
+
+client = Client()
+
+app = Flask(__name__)
+
+to = "+13234924416"
+from_ = "+17867892968"
+message_body = "Hello from Haseeb ur Rahman CS643 Fall 2017"
+
+@app.route("/")
+def hello():
+    #call = client.calls.create(
+    #        to="+13234924416",
+    #        from_="+17867892968",
+    #        url="https://demo.twilio.com/welcome/voice/"
+    #    )
+
+    message = client.messages.create(
+            to=to,
+            from_=from_,
+            body=message_body
+            )
+    return "Message ID: {} sent to {} from {}".format(message.sid, to, from_)
+
+app.run(host='0.0.0.0', port=80)
